56d02df57372a980e2629d549c965fd630e2ab41
AVINASH PANDEY <avinash24092@gmail.com>
2025-03-19 17:42:27 +0530

    Avinash Branch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { Suspense, useState, useEffect, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
